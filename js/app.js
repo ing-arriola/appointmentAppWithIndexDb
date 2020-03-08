76aa54ae8d7f0d3670a46f7c16e2e02e929e413a
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-08 15:27:28 -0600

    Added selectors of form

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let DB
+
+const form=document.querySelector('form'),
+      petName=document.querySelector('#mascota'),
+      customerName=document.querySelector('#ciente'),
+      phone=document.querySelector('#telefono'),
+      date=document.querySelector('#fecha'),
+      hour=document.querySelector('#hora'),
+      symphtoms=document.querySelector('#sintomas'),
+      meetings=document.querySelector('#citas'),
+      headManage=document.querySelector('#administra')
